dc3a332b5d23c6179bca2f016b4e0d26fadae79b
David C Connolly <bgp.studios.hou@gmail.com>
2026-04-04 19:54:18 -0500

    Initial Commit

---
+++ IntroTests/IntroTests.swift
@@ -0,0 +1,18 @@
+//
+//  IntroTests.swift
+//  IntroTests
+//
+//  Created by Sean Connolly on 4/4/26.
+//
+
+import Testing
+
+struct IntroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
